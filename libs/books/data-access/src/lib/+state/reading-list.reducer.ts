4891b4590e625c61fb53662ad09c964e3894fd5d
Kinnera <kinnera.bezawada2@t-mobile.com>
2023-11-26 14:25:44 -0500

    Task 1 - add code review and fix tests

--- libs/books/data-access/src/lib/+state/reading-list.reducer.ts
+++ libs/books/data-access/src/lib/+state/reading-list.reducer.ts
@@ -35,24 +35,30 @@ const readingListReducer = createReducer(
       error: null
     };
   }),
-  on(ReadingListActions.loadReadingListSuccess, (state, action) => {
-    return readingListAdapter.setAll(action.list, {
+  on(ReadingListActions.loadReadingListSuccess, (state, { list }) => {
+    return readingListAdapter.setAll(list, {
       ...state,
       loaded: true
     });
   }),
-  on(ReadingListActions.loadReadingListError, (state, action) => {
+  on(ReadingListActions.loadReadingListError, (state, { error }) => {
     return {
       ...state,
-      error: action.error
+      error
     };
   }),
-  on(ReadingListActions.addToReadingList, (state, action) =>
-    readingListAdapter.addOne({ bookId: action.book.id, ...action.book }, state)
+  on(ReadingListActions.addToReadingList, (state, { book }) =>
+    readingListAdapter.addOne({ bookId: book.id, ...book }, state)
   ),
-  on(ReadingListActions.removeFromReadingList, (state, action) =>
-    readingListAdapter.removeOne(action.item.bookId, state)
-  )
+  on(ReadingListActions.removeFromReadingList, (state, { item }) =>
+  readingListAdapter.removeOne(item.bookId, state)
+  ),
+  on(ReadingListActions.failedAddToReadingList, (state, { book }) =>
+    readingListAdapter.removeOne(book.id, state)
+  ),
+  on(ReadingListActions.failedRemoveFromReadingList, (state, { item }) =>
+    readingListAdapter.addOne(item, state)
+    )
 );
 
 export function reducer(state: State | undefined, action: Action) {
